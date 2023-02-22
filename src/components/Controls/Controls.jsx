c9e1104456aa2ae23352c848aa71a8a07f8a998a
minjaeleee <dlalswo4602@naver.com>
2023-02-22 10:41:28 +0900

    shuffle 기능 추가

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,4 +1,8 @@
 import React, { memo, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import RepeatIcon from "@mui/icons-material/Repeat"
+import RepeatOneIcon from "@mui/icons-material/RepeatOne"
 import PauseIcon from "@mui/icons-material/Pause";
 import SkipPrevious from "@mui/icons-material/SkipPrevious";
 import PlayArrow from "@mui/icons-material/PlayArrow";
@@ -6,9 +10,23 @@ import SkipNext from "@mui/icons-material/SkipNext";
 import QueueMusic from "@mui/icons-material/QueueMusic";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+
+import { nextMusic, prevMusic, setRepeatMusic } from "../../store/musicPlayerReducer";
 import "./Controls.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { nextMusic, prevMusic } from "../../store/musicPlayerReducer";
+
+const RepeatButton = ({repeat, ...props}) => {
+  console.log("props", props)
+  switch(repeat) {
+    case 'ALL':
+      return <RepeatIcon sx={{fontSize:30, cursor:"pointer"}} {...props}/>
+    case 'ONE':
+      return <RepeatOneIcon sx={{fontSize:30, cursor:"pointer"}} {...props}/>
+    case 'SHUFFLE':
+      return <ShuffleIcon sx={{fontSize:30, cursor:"pointer"}} {...props}/>
+    default: 
+      return null;
+  }
+}
 
 const Controls = ({
   showMusicList,
@@ -19,6 +37,7 @@ const Controls = ({
   changeVolume
 }) => {
   const playing = useSelector(state=>state.playing)
+  const repeat = useSelector(state=>state.repeat)
   const dispatch = useDispatch()
 
   const onClickPlay = () => play()
@@ -30,11 +49,19 @@ const Controls = ({
     changeVolume(e.target.value)
   }
 
+  const onClickRepeat = () => {
+    dispatch(setRepeatMusic())
+  }
+
   return (
     <div className="control-area">
       <QueueMusic
         sx={{ fontSize: 30, cursor: "pointer" }}
       />
+      {/* <RepeatIcon sx={{fontSize:30, cursor:"pointer"}}></RepeatIcon>
+      <RepeatOneIcon />
+      <ShuffleIcon /> */}
+      <RepeatButton repeat={repeat} onClick={onClickRepeat}/>
       <SkipPrevious
         sx={{ fontSize: 30, cursor: "pointer" }}
         onClick={onClickPrev}
